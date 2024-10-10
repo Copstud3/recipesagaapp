21b2c5cad9f0cb0d377bdfdf0c15bfaf51efc7cd
Chris <victorchris73@gmail.com>
2024-10-10 20:37:41 +0100

    bug fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import styles from "./footer.module.css"
 
 export default function Footer(){
     return <div>
-         <p className={styles.footer}>Made by <a href="http://linktr.ee/iam_chris" target="_blank">Chris</a></p>
+         <p className={styles.footer}>Made by <a href="http://linktr.ee/iam_chris" target="_blank">Chris</a> - 2024</p>
     </div>
 }
  
