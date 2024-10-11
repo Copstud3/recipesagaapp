ce1e6182a7d03551faaf57fc70b6419c2392172d
Chris <victorchris73@gmail.com>
2024-10-11 13:48:47 +0100

    design fix

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 import styles from './welcome.module.css'
 
 export default function Welcome() {
-    return <div>
+    return <div className={styles.welcomeContainer}>
         <p className={styles.welcomeText}>Welcome to <span className={styles.logoName}>RecipeSaga</span>, Your Culinary Adventure Awaits!</p>
         <p className={styles.welcomeSubText}>Embark on a delicious journey with RecipeSaga, where every meal is an opportunity to create something extraordinary. Our vast collection of recipes is just waiting to inspire your next culinary masterpiece!</p>
     </div>
